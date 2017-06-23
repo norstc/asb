4ad002fa4bf126325c90a405a7f4b5b68a673c9f
norstc <norstc@gmail.com>
2017-06-23 09:29:45 +0800

    initial

---
+++ src/main/java/com/norstc/asb/AsbApplication.java
@@ -0,0 +1,12 @@
+package com.norstc.asb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsbApplication.class, args);
+	}
+}
