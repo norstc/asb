6c3042b20257cbd1a61fa7c95d414f86c4555a42
norstc <norstc@gmail.com>
2017-06-25 22:13:50 +0800

    welcome home page

--- src/main/java/com/norstc/asb/controller/WelcomeController.java
+++ src/main/java/com/norstc/asb/controller/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 	@RequestMapping("/")
 	public String welcomeHandler(){
-		return "index";
+		return "welcome";
 	}
 }
