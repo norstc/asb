4a82abcef9baf9871467f147b5de8d738f36718c
norstc <norstc@gmail.com>
2017-06-28 10:28:26 +0800

    all html templates and basic controllers

--- src/main/java/com/norstc/asb/controller/WelcomeController.java
+++ src/main/java/com/norstc/asb/controller/WelcomeController.java
@@ -19,4 +19,59 @@ public class WelcomeController {
 	public String tempHandler(){
 		return "temp";
 	}
+	
+	@RequestMapping("/aboutus")
+	public String aboutUsHandler(){
+		return "aboutus";
+	}
+	
+	@RequestMapping("/help")
+	public String helpHandler(){
+		return "help";
+	}
+	
+	@RequestMapping("/main/select")
+	public String mainSelectHandler(){
+		return "main/select";
+	}
+	
+	@RequestMapping("/main/target")
+	public String mainTargetHandler(){
+		return "main/target";
+	}
+	
+	@RequestMapping("main/recorder")
+	public String mainRecorderHandler(){
+		return "main/recorder";
+	}
+	
+	@RequestMapping("main/balance")
+	public String mainBalanceHandler(){
+		return "main/balance";
+	}
+	
+	@RequestMapping("main/another")
+	public String mainAnotherHandler(){
+		return "main/another";
+	}
+	
+	@RequestMapping("main/secret")
+	public String mainSecretHandler(){
+		return "main/secret";
+	}
+	
+	@RequestMapping("user/login")
+	public String userLoginHandler(){
+		return "user/login";
+	}
+	
+	@RequestMapping("user/logout")
+	public String userLogoutHandler(){
+		return "user/logout";
+	}
+	
+	@RequestMapping("user/regist")
+	public String userRegistHandler(){
+		return "user/regist";
+	}
 }
