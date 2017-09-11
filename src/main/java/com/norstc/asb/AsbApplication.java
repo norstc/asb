9380e0963fa2a12775c86116a852b46dfcccce17
norstc <norstc@gmail.com>
2017-09-11 20:14:27 +0800

    new branch for war deployment

--- src/main/java/com/norstc/asb/AsbApplication.java
+++ src/main/java/com/norstc/asb/AsbApplication.java
@@ -2,10 +2,16 @@ package com.norstc.asb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AsbApplication {
-
+public class AsbApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(AsbApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AsbApplication.class, args);
 	}
